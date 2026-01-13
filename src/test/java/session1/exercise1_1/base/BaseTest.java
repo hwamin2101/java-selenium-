f9eb1f94d637af350c7168f6e978e460b4b7deaa
hwamin2101 <kimnamjoon2101@gmail.com>
2026-01-13 23:54:30 +0700

    Setup BaseTest and ConfigReader using properties for reuse

--- src/test/java/session1/exercise1_1/base/BaseTest.java
+++ src/test/java/session1/exercise1_1/base/BaseTest.java
@@ -1,26 +1,26 @@
-package session1.exercise1_1;
+package session1.exercise1_1.base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import session1.exercise1_1.utils.ConfigReader;
 
-public class FirstTest {
+public class BaseTest {
 
-    WebDriver driver;
+    protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
+        String browser = ConfigReader.get("browser");
 
-    @Test
-    public void openGoogle() {
-        driver.get("https://www.google.com");
+        if ("chrome".equalsIgnoreCase(browser)) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+
+        driver.manage().window().maximize();
     }
 
     @AfterMethod
