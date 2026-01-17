f71f9d7ca41a4fa6e617ae41c83b8ce5a53fc12b
hwamin2101 <kimnamjoon2101@gmail.com>
2026-01-18 00:50:06 +0700

    Exercise 1.3: Login flow, element interactions and window handling

---
+++ src/test/java/session1/exercise1_3/tests/LoginTest.java
@@ -0,0 +1,63 @@
+package session1.exercise1_3.tests;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import session1.base.BaseTest;
+import session1.exercise1_3.locators.LoginLocators;
+import session1.utils.ConfigReader;
+
+import java.time.Duration;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void verifyLoginSuccessfully() {
+
+        driver.get(ConfigReader.get("ex3.url"));
+
+        driver.findElement(LoginLocators.USERNAME_INPUT)
+                .sendKeys(ConfigReader.get("ex3.username"));
+
+        driver.findElement(LoginLocators.PASSWORD_INPUT)
+                .sendKeys(ConfigReader.get("ex3.password"));
+
+        driver.findElement(LoginLocators.SUBMIT_BUTTON).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement successMessage = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        LoginLocators.SUCCESS_MESSAGE
+                )
+        );
+
+        Assert.assertEquals(
+                successMessage.getText(),
+                ConfigReader.get("ex3.success.text")
+        );
+
+
+        String parentWindow = driver.getWindowHandle();
+        String successUrl = driver.getCurrentUrl();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get(successUrl);
+
+        WebElement newTabSuccess = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        LoginLocators.SUCCESS_MESSAGE
+                )
+        );
+
+        Assert.assertEquals(
+                newTabSuccess.getText(),
+                ConfigReader.get("ex3.success.text")
+        );
+
+        driver.switchTo().window(parentWindow);
+    }
+
+}
