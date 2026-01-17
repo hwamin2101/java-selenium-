1ec2ab102eef2a88e707d68ae581a148d5db9531
hwamin2101 <kimnamjoon2101@gmail.com>
2026-01-18 01:08:44 +0700

    Exercise 1.4: Handle dynamic loading with explicit and custom waits

---
+++ src/test/java/session1/utils/WaitUtils.java
@@ -0,0 +1,21 @@
+package session1.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitUtils {
+
+    public static WebDriverWait getWait(WebDriver driver) {
+        int timeout = Integer.parseInt(ConfigReader.get("explicit.wait"));
+        return new WebDriverWait(driver, Duration.ofSeconds(timeout));
+    }
+
+    public static ExpectedCondition<Boolean> elementInvisible(By locator) {
+        return driver -> driver.findElements(locator).isEmpty()
+                || !driver.findElement(locator).isDisplayed();
+    }
+}
