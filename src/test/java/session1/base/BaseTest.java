dbd95ae160e2cde82be86bece9506db9861708db
hwamin2101 <kimnamjoon2101@gmail.com>
2026-01-17 23:36:40 +0700

    Fix CI: run Chrome in headless mode

--- src/test/java/session1/base/BaseTest.java
+++ src/test/java/session1/base/BaseTest.java
@@ -3,6 +3,7 @@ package session1.base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import session1.utils.ConfigReader;
@@ -17,7 +18,14 @@ public class BaseTest {
 
         if ("chrome".equalsIgnoreCase(browser)) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
         }
 
         driver.manage().window().maximize();
